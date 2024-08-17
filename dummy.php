eabebb44066bbb2fd3c5d7cedd83a08c3ed2ac7b
Timothy Lin <100029142@mvla.net>
2024-08-17 13:09:21 -0700

    testing if I can pull php information from array

--- dummy.php
+++ dummy.php
@@ -2,5 +2,10 @@
 //echo post
 $name=$_GET['name'];
 $URL=$_GET('url');
+$debaterInformation = array(
+    'name' => 'John',
+    'school' => 30,
+    'url' => 'New York'
+  );
 echo json_encode($name);
 
