c1d84a5b2306698607628c24d07d13bc3324e9b5
Timothy Lin <100029142@mvla.net>
2024-08-19 15:39:44 -0700

    unjsoning it

--- dummy.php
+++ dummy.php
@@ -8,7 +8,8 @@ $debaterInformation = array(
     'url' => 'New York'
   );
 
-echo json_encode($debaterInformation);
+echo $debaterInformation;
+//json_encode($debaterInformation);
 //echo "<script>console.log('This will be logged to the console');</script>";
 
 
