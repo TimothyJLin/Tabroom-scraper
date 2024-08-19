8da45a221c5d747aca06735b0e677969211e2fa6
Timothy Lin <100029142@mvla.net>
2024-08-18 21:38:30 -0700

    readded data values

--- dummy.php
+++ dummy.php
@@ -1,14 +1,14 @@
 <?php
 //echo post
-#$name=$_GET['name'];
-#$URL=$_GET('url');
+$name=$_GET['name'];
+$URL=$_GET('url');
 $debaterInformation = array(
     'name' => 'John',
     'school' => 30,
     'url' => 'New York'
   );
 
+echo json_encode($debaterInformation);
 echo "<script>console.log('This will be logged to the console');</script>";
 
-echo json_encode($debaterInformation);
 
