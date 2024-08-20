92a0d5019bf5d49469d6ed5dc51627c1620a412c
Timothy Lin <100029142@mvla.net>
2024-08-20 16:24:58 -0700

    pulling data

--- dummy.php
+++ dummy.php
@@ -14,7 +14,6 @@ $debaterInformation = array(
   );
 
 return $debaterInformation;
-
 //json_encode($debaterInformation);
 //echo "<script>console.log('This will be logged to the console');</script>";
 
