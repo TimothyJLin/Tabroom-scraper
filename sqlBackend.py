5aab2d7b1734b850f202d12860e76e9c1e346a82
Timothy Lin <100029142@mvla.net>
2024-08-25 22:20:17 -0700

    pushing files and working on them at glitch

--- sqlBackend.py
+++ sqlBackend.py
@@ -2,14 +2,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="mysql-project1-records-of-debaters.g.aivencloud.com",
   user="avnadmin",
-  password="AVNS_4XSzs7y1KtaqGb4e-V3",
+  password="[password]",
   database="defaultdb",
   port=21468,
 #environ['password'].
 )
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM localentries WHERE name='shion lee'")
+mycursor.execute("SELECT * FROM localentries WHERE name='Timothy Lin'")
 
 myresult = mycursor.fetchall()
 
